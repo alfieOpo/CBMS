ab0de907b0b7771974d82dc5196cb0889871774e
alfieOpo <opofiela@gmail.com>
2017-04-18 15:58:35 +0800

    Alfie Commit

--- app/src/main/java/absnitsolution/modifiedcommunitybasemonitoringsystem/_X_Question.java
+++ app/src/main/java/absnitsolution/modifiedcommunitybasemonitoringsystem/_X_Question.java
@@ -27,11 +27,12 @@ public class _X_Question extends Fragment {
 
     MainDataBaseHandler da;
     ImageButton btn_back, btn_next;
-    LinearLayout layout,layout130;
-    MaterialBetterSpinner cbo_x_127,cbo_x_128,cbo_127_1,cbo_x_129_1,cbo_x_129_2;
-    EditText txt_x_127_1,txt_x_127_2,txt_x_126_8,txt_x_132_16,txt_x_130_3;
+    LinearLayout layout,layout130,layout126,layout30,layout131,layout132;
+    MaterialBetterSpinner cbo_x_127,cbo_x_128,cbo_127_1,cbo_x_129_1,cbo_x_129_2,cbo_x_125;
+    EditText txt_x_127_1,txt_x_127_2,txt_x_126_8,txt_x_132_16,txt_x_130_3,txt_x_124;
     CheckBox chk_x_126_8,chk_x_132_16,chk_x_130_3;
     boolean falseall=false;
+    View view;
     public _X_Question() {
     }
 
@@ -40,36 +41,59 @@ public class _X_Question extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment___x__question, container, false);
+        view = inflater.inflate(R.layout.fragment___x__question, container, false);
         btn_back = (ImageButton) view.findViewById(R.id.btn_back);
         btn_next = (ImageButton) view.findViewById(R.id.btn_next);
-        layout130=(LinearLayout)view.findViewById(R.id.layout130);
+
         layout=(LinearLayout)view.findViewById(R.id.layout);
         cbo_x_127=(MaterialBetterSpinner)view.findViewById(R.id.cbo_x_127);
         cbo_127_1=(MaterialBetterSpinner)view.findViewById(R.id.cbo_127_1);
+        layout126=(LinearLayout)view.findViewById(R.id.layout126);
         cbo_x_128=(MaterialBetterSpinner)view.findViewById(R.id.cbo_x_128);
         cbo_x_129_1=(MaterialBetterSpinner)view.findViewById(R.id.cbo_x_128);
         cbo_x_129_2=(MaterialBetterSpinner)view.findViewById(R.id.cbo_x_128);
         chk_x_132_16=(CheckBox)view.findViewById(R.id.chk_x_132_16);
         chk_x_126_8=(CheckBox)view.findViewById(R.id.chk_x_126_8);
         chk_x_130_3=(CheckBox)view.findViewById(R.id.chk_x_130_3);
-
+        cbo_x_125=(MaterialBetterSpinner)view.findViewById(R.id.cbo_x_125);
         txt_x_126_8=(EditText)view.findViewById(R.id.txt_x_126_8);
         txt_x_132_16=(EditText)view.findViewById(R.id.txt_x_132_16);
         txt_x_130_3=(EditText)view.findViewById(R.id.txt_x_130_3);
+        txt_x_124=(EditText)view.findViewById(R.id.txt_x_124);
+        layout130=(LinearLayout)view.findViewById(R.id.layout130);
+        layout131=(LinearLayout)view.findViewById(R.id.layout131);
+        layout132=(LinearLayout)view.findViewById(R.id.layout132);
         da=new MainDataBaseHandler(getActivity());
         if(da._120OO(Config.ID)){
-            layout.setVisibility(View.VISIBLE);
+
+            cbo_x_129_1.setVisibility(View.VISIBLE);
+            cbo_x_129_2.setVisibility(View.VISIBLE);
+            cbo_127_1.setVisibility(View.VISIBLE);
+            txt_x_124.setVisibility(View.VISIBLE);
+             cbo_x_125.setVisibility(View.VISIBLE);
+            layout130.setVisibility(View.VISIBLE);
+            layout126.setVisibility(View.VISIBLE);
+            layout131.setVisibility(View.VISIBLE);
+            layout132.setVisibility(View.VISIBLE);
+
         }
         else {
-            layout.setVisibility(View.INVISIBLE);
+            cbo_x_129_1.setVisibility(View.INVISIBLE);
+            cbo_x_129_2.setVisibility(View.INVISIBLE);
+            cbo_127_1.setVisibility(View.INVISIBLE);
+            txt_x_124.setVisibility(View.INVISIBLE);
+            cbo_x_125.setVisibility(View.INVISIBLE);
+            layout130.setVisibility(View.INVISIBLE);
+            layout126.setVisibility(View.INVISIBLE);
+            layout131.setVisibility(View.INVISIBLE);
+            layout132.setVisibility(View.INVISIBLE);
 
         }
         this.cpar = new c_params(Config.ID, container, view);
 
 
         this.cpar.setEditText(R.id.txt_x_124);
-        this.cpar.setDropdown(R.id.cbo_x_125, R.array.x_kumpara, "Select One");
+        this.cpar.setDropdown(R.id.cbo_x_125, R.array.x_kumpara, "");
         this.cpar.setDropdown(R.id.cbo_127_1, R.array.oo_hindi, "Hindi");
         this.cpar.setDropdown(R.id.cbo_x_127, R.array.oo_hindi, "Hindi");
         this.cpar.setDropdown(R.id.cbo_x_128, R.array.x_128, "Ang dating binhi ay mataas ang halaga");
@@ -247,7 +271,41 @@ public class _X_Question extends Fragment {
 
             }
         });
+        cbo_x_125.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position!=0){
+                    layout126.setVisibility(View.INVISIBLE);
+                    Clearthis(R.id.chk_x_126_1);
+                    Clearthis(R.id.chk_x_126_2);
+                    Clearthis(R.id.chk_x_126_3);
+                    Clearthis(R.id.chk_x_126_4);
+                    Clearthis(R.id.chk_x_126_5);
+                    Clearthis(R.id.chk_x_126_6);
+                    Clearthis(R.id.chk_x_126_7);
+                    Clearthis(R.id.chk_x_126_8);
 
+                }
+                else{
+                    layout126.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        if(!cbo_x_125.getText().toString().equals("nabawasan")){
+            layout126.setVisibility(View.INVISIBLE);
+            Clearthis(R.id.chk_x_126_1);
+            Clearthis(R.id.chk_x_126_2);
+            Clearthis(R.id.chk_x_126_3);
+            Clearthis(R.id.chk_x_126_4);
+            Clearthis(R.id.chk_x_126_5);
+            Clearthis(R.id.chk_x_126_6);
+            Clearthis(R.id.chk_x_126_7);
+            Clearthis(R.id.chk_x_126_8);
+
+        }
+        else{
+            layout126.setVisibility(View.VISIBLE);
+        }
 ///------------------------------------------------------------
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -347,6 +405,14 @@ public class _X_Question extends Fragment {
         this.cpar.putEditText(R.id.txt_x_132_16);
         da.c_Update(cpar);
         super.onDestroy();
+    }
+
+    public void Clearthis(int R){
+
+
+        try{CheckBox box=(CheckBox)view.findViewById(R); box.setChecked(false);}catch (Exception xx){}
+        try{EditText text=(EditText)view.findViewById(R); text.setText("");}catch (Exception xx){}
+
     }
 }
 
